1efb2f7b2feb58cfca5e91a825a2854828c38ce2
Vita Kostiuk <vitagrebennik@gmail.com>
2022-07-08 12:48:46 +0300

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from '../Sidebar';
+import Main from '../Main';
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
